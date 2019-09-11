aef2e9fd3fb8761495df1010509b145b3d2222fa
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2019-09-11 21:28:01 +0200

    Add files via upload

--- raffle.js
+++ raffle.js
@@ -1,54 +1,87 @@
 function raffleDraw(object) {
 
-	if ( typeof object === "object" ) {
+  if ( typeof object === "object" ) {
 
-		const { amountOfTickets, amountOfWinningTickets, winningTickets } = object;
+    const { amountOfTickets, amountOfWinningTickets, winningTickets, events } = object;
 
-		const a = amountOfTickets;
-		let b = typeof amountOfWinningTickets === "number" ? amountOfWinningTickets : void 0,
-		c = typeof winningTickets !== "undefined" ? winningTickets : void 0;
+    const a = amountOfTickets,
+    random = () => Math.floor( Math.random() * a + 1 );
+    let b = typeof amountOfWinningTickets === "number" ? amountOfWinningTickets : void 0,
+    c = typeof winningTickets !== "undefined" ? winningTickets : void 0;
 
-		if ( typeof ( a + b ) === "number" && Number.isInteger( a + b ) && b <= a || typeof a === "number" && Array.isArray(c) && c.length <= a ) {
+    if ( typeof ( a + b ) === "number" && Number.isInteger( a + b ) && b <= a || typeof a === "number" && Array.isArray(c) && c.length <= a ) {
 
-			const random = () => Math.floor( Math.random() * a + 1 );
+      c = typeof c !== "undefined" && typeof c.reduce( ( a, b ) => a + b ) === "number" ? c : [];
 
-			c = typeof c !== "undefined" && typeof c.reduce( ( a, b ) => a + b ) === "number" ? c : [];
+      c.forEach( v => { if ( ( Number.isInteger(v) === true && v >= 0 && v <= a && c.slice( c.indexOf(v) + 1 ).indexOf(v) === -1 ) === false ) { throw TypeError(`\`${v}' is an invalid ticket number.`) } } );
 
-			c.forEach( v => { if ( ( Number.isInteger(v) === true && v >= 0 && v <= a && c.slice( c.indexOf(v) + 1 ).indexOf(v) === -1 ) === false ) { throw TypeError(`\`${v}' is an invalid ticket number.`) } } );
+      b = c.length > 0 ? c.length : b;
 
-			b = c.length > 0 ? c.length : b;
+      if ( c.length === 0 ) {
 
-			const winPct = Number( ( b / ( a / 100 ) ).toFixed(2) );
+        for ( let i = 0; i < b; i++ ) {
 
-			if ( c.length === 0 ) {
+          let number = random();
 
-				for ( let i = 0; i < b; i++ ) {
+          while ( c.includes( number ) ) {
 
-					let number = random();
+            number = random();
 
-					while ( c.includes( number ) ) {
+          }
 
-						number = random();
+          c.push( number );
 
-					}
+        }
 
-					c.push( number );
+      }
 
-				}
+      if ( c.length > 0 ) {
 
-			}
+        const drawnTicket = random(),
+        isWinning = c.includes( drawnTicket );
 
-			if ( c.length > 0 ) {
+        if ( typeof events !== "undefined" && Array.isArray(events) ) {
 
-				const drawnTicket = random(),
-				isWinning = c.includes( drawnTicket );
+          events.forEach( v => {
 
-				return { isWinning, winningTickets: c, drawnTicket, winPct };
+            const requiredTickets = v.requiredTickets,
+            callback = v.callback;
 
-			}
+            if ( Array.isArray( requiredTickets ) && requiredTickets.length > 0 && requiredTickets.length <= 2 && typeof callback === "function" ) {
 
-		}
+              const isNumber = typeof requiredTickets.reduce( ( a, b ) => a + b ) === "number",
+              isBoolean = typeof requiredTickets === "boolean";
 
-	}
+              if ( isNumber && requiredTickets.length === 1 && drawnTicket === requiredTickets[0] || isNumber && requiredTickets.length === 2 && drawnTicket >= requiredTickets[0] && drawnTicket <= requiredTickets[1] || isBoolean && requiredTickets === isWinning ) {
+
+                callback();
+
+              }
+
+            }
+
+          });
+
+        }
+
+        return { isWinning, winningTickets: c, drawnTicket };
+
+      }
+
+    }
+
+  }
 
 }
+
+raffleDraw({ 
+  amountOfTickets: 100, 
+  amountOfWinningTickets: 10, 
+  events: [
+
+    { requiredTickets: [0,10], callback(){ console.log("10%") } },
+    { requiredTickets: [11,50], callback(){ console.log("50%") } },
+    { requiredTickets: [51,100], callback(){ console.log("100%") } }
+
+  ] 
+});
